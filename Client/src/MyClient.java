6c92252b705ed5241224dcefeddce6c3cb9c9953
Jakub Kurczak <kubakurczak97@gmail.com>
2021-03-22 08:43:59 +0100

    poprawiony klient

--- Client/src/MyClient.java
+++ Client/src/MyClient.java
@@ -1,12 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class MyClient {
-    static final String url_name = "//localhost/MyService";
-    static final String url_name_2 = "//localhost/MyService2";
+    static final String url_name = "//25.28.138.108:1098/MyService";
+    static final String url_name_2 = "//25.28.138.108:1098/MyService";
     public static void main(String[] args){
         CalcObject2 calcObject2 = null;
         ResultType2 resultType = null;
-
+        MyClient.hello();
 
         if(System.getSecurityManager() == null)
             System.setSecurityManager(new SecurityManager());
@@ -43,6 +45,18 @@ public class MyClient {
         }
         System.out.println("Wynik wielomianu to: " + result);
 
+
+    }
+
+    static void hello(){
+        //imie nazwisko nr albumu
+        //data i czas do sekund
+        //username
+        System.out.println("Jakub Kurczak 237470");
+        System.out.println("Maciej Rys 246648");
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        System.out.println(System.getProperty("user.name"));
 
     }
 }
