eec47829feb26e50615bc4bbb5e0b0c2f3780270
Maciej <rusekk666@gmail.com>
2021-03-29 08:01:50 +0200

    client working with local

--- GrpcGreeterClient/GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -9,8 +9,19 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
+            //Używamy frameworka .net 5.0
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine("Maciej Ryś");
+            Console.WriteLine("Jakub Kurczak");
+            var httpHandler = new HttpClientHandler();
+            // Return true to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+
+            using var channel = GrpcChannel.ForAddress("https://25.28.162.104:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
             //the port number(5001) must match the port of the grpc server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //using var channel = GrpcChannel.ForAddress("https://25.28.162.104:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "greeterclient" });
