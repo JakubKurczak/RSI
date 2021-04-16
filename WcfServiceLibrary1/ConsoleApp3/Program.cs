b5a1c4cf914db3f949594f63a3a12beb72e4e509
Jakub Kurczak <kubakurczak97@gmail.com>
2021-04-16 14:39:01 +0200

    actually works

---
+++ WcfServiceLibrary1/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3.ServiceReference1;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KalkulatorClient kalkulatorClient = new KalkulatorClient();
+
+            double result = kalkulatorClient.Dodaj(2.3, 3.3);
+            Console.WriteLine("Wynik: " + result);
+            Console.ReadLine();
+            kalkulatorClient.Close();
+        }
+    }
+}
