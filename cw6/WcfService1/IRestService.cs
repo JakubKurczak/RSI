ca4732f0ae1c761121ba001056968f6b2a895139
Maciej <rusekk666@gmail.com>
2021-05-09 19:13:31 +0200

    Cw6 initial

---
+++ cw6/WcfService1/IRestService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+//http://localhost:56384/Service1.svc
+namespace WcfService1
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę interfejsu „IService1” w kodzie i pliku konfiguracji.
+    [ServiceContract]
+    public interface IRestService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        Book GetDataUsingDataContract(Book composite);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/Books")]
+        List<Book> getAllXml();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/Books")]
+        List<Book> getAllJson();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/Books/{id}", ResponseFormat = WebMessageFormat.Xml)]
+        Book getByIdXml( string Id);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/Books/{id}", ResponseFormat = WebMessageFormat.Json)]
+        Book getByIdJson(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Books/{id}", Method = "PUT", RequestFormat = WebMessageFormat.Xml)]
+        string updateByIdXml(string Id, string Title, string Author, int Pages);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Books/{id}", Method = "PUT", RequestFormat = WebMessageFormat.Json)]
+        string updateByIdJson(string Id, string Title, string Author, int Pages);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Books/{id}", Method = "PUT", RequestFormat = WebMessageFormat.Xml)]
+        string borrowBookXml(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Books/{id}", Method = "PUT", RequestFormat = WebMessageFormat.Json)]
+        string borrowBookJson(string Id);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Books", Method = "POST", RequestFormat =WebMessageFormat.Xml)]
+        string addXml(Book book);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Books", Method = "POST", RequestFormat = WebMessageFormat.Json)]
+        string addJson(Book book);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Books/{id}", Method = "DELETE")]
+        string deleteXml(Book book);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Books/{id}", Method = "DELETE")]
+        string deleteJson(Book book);
+        // TODO: dodaj tutaj operacje usługi
+    }
+
+
+
+    // Użyj kontraktu danych, jak pokazano w poniższym przykładzie, aby dodać typy złożone do operacji usługi.
+    [DataContract]
+    public class Book
+    {
+        static int idCounter = 0;
+        public int id = 0;
+        bool isBorrow = false;
+        string title = "";
+        string author = "";
+        int pages = 0;
+
+        public Book( string title, string author, int pages)
+        {
+            this.id = idCounter;
+            idCounter++;
+            this.Author = author;
+            this.Pages = pages;
+            this.Title = title;
+        }
+
+        [DataMember(Order = 0)]
+        public bool IsBorrow
+        {
+            get { return isBorrow; }
+            set { isBorrow = value; }
+        }
+
+        [DataMember(Order = 1)]
+        public int Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
+
+        [DataMember(Order = 2)]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        [DataMember(Order = 3)]
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+    }
+
+}
+
---
+++ cw6/WcfService1/Service1.svc.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „Service1” w kodzie, usłudze i pliku konfiguracji.
+    // UWAGA: aby uruchomić klienta testowego WCF w celu przetestowania tej usługi, wybierz plik Service1.svc lub Service1.svc.cs w eksploratorze rozwiązań i rozpocznij debugowanie.
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.Single)]
+    public class Service1 : IRestService
+    {
+        private static List<Book> Books = new List<Book> { new Book("Lalka", "Bolesław Prus", 560), new Book("Tango", "Sławomir Mrożek", 200) };
+
+        public string addJson(Book book)
+        {
+            Books.Add(book);
+            return "książka została dodana";
+        }
+
+        public string addXml(Book book)
+        {
+            Books.Add(book);
+            return "książka została dodana";
+        }
+
+        public string deleteJson(Book book)
+        {
+            if (Books.Remove(book))
+            {
+                return "książka została usunięta";
+            }
+            else
+            {
+                return "książka nie istnieje";
+            }
+        }
+
+        public string deleteXml(Book book)
+        {
+            if (Books.Remove(book))
+            {
+                return "książka została usunięta";
+            }
+            else
+            {
+                return "książka nie istnieje";
+            }
+
+        }
+
+        public List<Book> getAllJson()
+        {
+            return Books;
+        }
+
+        public List<Book> getAllXml()
+        {
+            return Books;
+        }
+
+        public Book getByIdJson(string Id)
+        {
+            return Books.Where(x => x.id == Int32.Parse(Id)).First();
+        }
+
+        public Book getByIdXml(string Id)
+        {
+            return Books.Where(x => x.id == Int32.Parse(Id)).First();
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Book GetDataUsingDataContract(Book composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+
+            return composite;
+        }
+
+        public string borrowBookJson(string Id)
+        {
+            var book = Books.Where(x => x.id == Int32.Parse(Id) && x.IsBorrow == false).First();
+            if (book != null)
+            {
+                book.IsBorrow = true;
+                return "książka wypożyczona";
+            }
+            else
+            {
+                return "nie można wypożyczyć";
+            }
+
+        }
+
+        public string borrowBookXml(string Id)
+        {
+            var book = Books.Where(x => x.id == Int32.Parse(Id) && x.IsBorrow == false).First();
+            if (book != null)
+            {
+                book.IsBorrow = true;
+                return "książka wypożyczona";
+            }
+            else
+            {
+                return "nie można wypożyczyć";
+            }
+        }
+
+        public string updateByIdJson(string Id, string Title, string Author, int Pages)
+        {
+            var book = Books.Where(x => x.id == Int32.Parse(Id)).First();
+            if (book != null)
+            {
+                book.Author = Author;
+                book.Pages = Pages;
+                book.Title = Title;
+                return "książka zaktualizowana";
+            }
+            else
+            {
+                return "książka nie istnieje";
+            }
+
+        }
+
+        public string updateByIdXml(string Id, string Title, string Author, int Pages)
+        {
+            var book = Books.Where(x => x.id == Int32.Parse(Id)).First();
+            if (book != null)
+            {
+                book.Author = Author;
+                book.Pages = Pages;
+                book.Title = Title;
+                return "książka zaktualizowana";
+            }
+            else
+            {
+                return "książka nie istnieje";
+            }
+        }
+    }
+}
